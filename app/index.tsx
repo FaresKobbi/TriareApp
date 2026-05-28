77c7af2c265a1febe02874e2bd887f56daff0b23
FaresKobbi <fares.kobbi@gmail.com>
2026-05-28 15:27:02 -0300

    refactor(ui): integrate useBluetoothConnection hook into index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,113 +1,80 @@
-import { View, Text, StyleSheet, ScrollView} from 'react-native'
-import React, { useState } from 'react'
-import { AppCard } from '@/src/components/ui/AppCard'
-import AppScreen from '@/src/components/ui/AppScreen'
-import { colors,shadows,spacing, typography } from '@/src/theme'
-import { BikeLogo } from '@/src/components/ui/BikeLogo'
 import { AppButton } from '@/src/components/ui/AppButton'
+import { AppCard } from '@/src/components/ui/AppCard'
 import { AppCardGradient } from '@/src/components/ui/AppCardGradient'
-import { Device } from '@/src/models/device'
 import { AppDeviceSelector } from '@/src/components/ui/AppDeviceSelector'
-import { requestBluetoothPermissions } from '@/src/features/bluetooth/hooks/requestBluetoothPermission'
-import { bluetoothService } from '@/src/features/bluetooth/BluetoothService'
+import AppScreen from '@/src/components/ui/AppScreen'
+import { BikeLogo } from '@/src/components/ui/BikeLogo'
+import { realBluetoothService } from '@/src/features/bluetooth/BluetoothService'
+import { ensureBluetoothPermissions } from '@/src/features/bluetooth/hooks/requestBluetoothPermission'
+import { useBluetoothConnection } from '@/src/features/bluetooth/hooks/useBluetoothConnection'
+import { TriareDeviceDTO } from '@/src/models/triareDeviceDTO'
+import { colors, shadows, spacing, typography } from '@/src/theme'
+import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 
 
 
 
 const Index = () => {
 
-  const [devices, setDevices] = useState<Device[]>([]);
+  const {
+    triareDevicesDTO,
+    status,
+    error,
+    startScan,
+    stopScan,
+    connect
+  } = useBluetoothConnection(realBluetoothService);
 
-  function scanDevices() {
+  ensureBluetoothPermissions();
 
-    bluetoothService.scanForDevice();
-    const foundDevices: Device[] = [
-      {
-        name: "TRIARE-001",
-        battery: 85,
-        signal: -45,
-      },
-      {
-        name: "TRIARE-002",
-        battery: 25,
-        signal: -57,
-      },
-      {
-        name: "TRIARE-003",
-        battery: 100,
-        signal: -85,
-      },
-      {
-        name: "TRIARE-004",
-        battery: null,
-        signal: -85,
-      },
-      {
-        name: "TRIARE-005",
-        battery: 5,
-        signal: null,
-      },
-      {
-        name: null,
-        battery: null,
-        signal: null,
-      },
-      {
-        name: "TRIARE-007",
-        battery: 5,
-        signal: -85,
-      }
-    ];
-
-    setDevices(foundDevices);
-  }
 
   return (
-    <AppScreen withBottomPadding={false} style = {styles.screen}>
-    
-        {/*Title*/}
-        <AppCard style = {styles.titleCard}>
-          <BikeLogo surfaceColor = {colors.primaryDark} bikeColor={colors.onPrimary}></BikeLogo>
-          <View>
-            <Text style = {typography.heroTitle}>Triare</Text>
-            <Text style = {styles.description}>Rehabilition tricycle control</Text>
-          </View>
-        </AppCard>
+    <AppScreen withBottomPadding={false} style={styles.screen}>
 
-        {/*Scanning Card*/}
-        <AppCardGradient 
-          colors={[colors.primarySoft, colors.onPrimary]}
-          containerStyle={[shadows.coloredShadow(colors.primarySoft), styles.scanningCardContainer]}
-          style={styles.scanningCard}>
-
-          <View>
-            <Text style = {typography.heroTitle}>Connect a tricycle</Text>
-            <Text style = {typography.heroTitle}>to begin a session</Text>
-          </View>
-          <Text style = {[styles.description, styles.scanningCardDescription]}>Make sure the device is powered on and within range. Bluetooth scanning stays active for one minute.</Text>
-          <AppButton 
-            title='Scan for devices'
-            textStyle = {styles.scanButtonTitle}
-            isShadowed = {true}
-            iconName='search'
-            onPress={scanDevices}
-          ></AppButton>
-        </AppCardGradient>
-
-        {/*Device List*/}
-        <View style = {styles.devicesCardTitle}>
-              <Text style = {[typography.body, {color: colors.textMuted}]}>AVAILABLE DEVICES</Text>
-              {displayDevicesCount(devices)}
+      {/*Title*/}
+      <AppCard style={styles.titleCard}>
+        <BikeLogo surfaceColor={colors.primaryDark} bikeColor={colors.onPrimary}></BikeLogo>
+        <View>
+          <Text style={typography.heroTitle}>Triare</Text>
+          <Text style={styles.description}>Rehabilition tricycle control</Text>
         </View>
+      </AppCard>
 
-        <ScrollView 
-          style = {styles.devicesCardContainer} 
-          contentContainerStyle = {styles.devicesCardContent}>
+      {/*Scanning Card*/}
+      <AppCardGradient
+        colors={[colors.primarySoft, colors.onPrimary]}
+        containerStyle={[shadows.coloredShadow(colors.primarySoft), styles.scanningCardContainer]}
+        style={styles.scanningCard}>
 
-          {devices.map((device)=>(
-            <AppDeviceSelector key={device.name} device={device} onPress={alert}></AppDeviceSelector>
-          ))}
-        </ScrollView>
+        <View>
+          <Text style={typography.heroTitle}>Connect a tricycle</Text>
+          <Text style={typography.heroTitle}>to begin a session</Text>
+        </View>
+        <Text style={[styles.description, styles.scanningCardDescription]}>Make sure the device is powered on and within range. Bluetooth scanning stays active for one minute.</Text>
+        <AppButton
+          title='Scan for devices'
+          textStyle={styles.scanButtonTitle}
+          isShadowed={true}
+          iconName='search'
+          onPress={startScan}
+        ></AppButton>
+      </AppCardGradient>
+
+      {/*Device List*/}
+      <View style={styles.devicesCardTitle}>
+        <Text style={[typography.body, { color: colors.textMuted }]}>AVAILABLE DEVICES</Text>
+        {displayDevicesCount(triareDevicesDTO)}
+      </View>
+
+      <ScrollView
+        style={styles.devicesCardContainer}
+        contentContainerStyle={styles.devicesCardContent}>
+
+        {triareDevicesDTO.map((device) => (
+          <AppDeviceSelector key={device.name} device={device} onPress={alert}></AppDeviceSelector>
+        ))}
+      </ScrollView>
 
     </AppScreen>
 
@@ -115,72 +82,72 @@ const Index = () => {
 }
 
 
-{/** HELPER */}
-function displayDevicesCount(devices: Device[]){
- return (
-    <Text style = {[typography.body, styles.description]}>{devices.length} found</Text>
- )
+{/** HELPER */ }
+function displayDevicesCount(devices: TriareDeviceDTO[]) {
+  return (
+    <Text style={[typography.body, styles.description]}>{devices.length} found</Text>
+  )
 }
 
 export default Index
 
 const styles = StyleSheet.create({
-  screen : {
-    gap : spacing.xs,
+  screen: {
+    gap: spacing.xs,
   },
 
-  description : {
-    color : colors.textLight,
+  description: {
+    color: colors.textLight,
   },
 
-  scanningCardDescription : {
-    fontSize : 16
+  scanningCardDescription: {
+    fontSize: 16
   },
 
-  scanButtonTitle : {
-    fontSize : 18,
+  scanButtonTitle: {
+    fontSize: 18,
     color: colors.onPrimary,
     fontWeight: "bold"
   },
 
-  titleCard: { 
+  titleCard: {
     flexDirection: "row",
-    alignItems : "center",
+    alignItems: "center",
     gap: spacing.sm,
-    flex : 0,
+    flex: 0,
     backgroundColor: colors.transparent,
     borderWidth: 0,
   },
-  
+
   scanningCard: {
-    flex : 5,
-    padding : spacing.lg,
-    gap : spacing.md,
+    flex: 5,
+    padding: spacing.lg,
+    gap: spacing.md,
     maxHeight: 250,
-    justifyContent : "center",
+    justifyContent: "center",
   },
 
-  scanningCardContainer:{
-    flex : 5,
+  scanningCardContainer: {
+    flex: 5,
     maxHeight: 250,
-    justifyContent : "center",
+    justifyContent: "center",
   },
 
-  devicesCardTitle:{
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"space-between",
+  devicesCardTitle: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     paddingHorizontal: spacing.lg,
     paddingTop: spacing.lg
   },
 
   devicesCardContainer: {
-    flex : 8,
+    flex: 8,
     backgroundColor: colors.transparent,
-    paddingHorizontal:spacing.md
+    paddingHorizontal: spacing.md
   },
-  
-  devicesCardContent:{
+
+  devicesCardContent: {
     gap: spacing.sm,
   }
 })
